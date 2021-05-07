524a70a3c5e8f1b2e82114af4584ff0af5474b8a
Ahmad Faaiz <ahmadfaaiz6@gmail.com>
2021-05-07 22:36:10 +0800

    Added demo (9) files

---
+++ Intro to RTOS - Exercises/9 Hardware Interrupts/ISR_semaphore_demo/ISR_semaphore_demo.ino
@@ -0,0 +1,101 @@
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+// Settings
+static const uint16_t timer_divider = 80; // Count at 1 MHz
+static const uint64_t timer_max_count = 1000000;
+
+// Pins
+static const int adc_pin = A0;
+
+// Globals
+static hw_timer_t *timer = NULL;
+static volatile uint16_t val;
+static SemaphoreHandle_t bin_sem = NULL;
+
+//*****************************************************************************
+// Interrupt Service Routines (ISRs)
+
+// This function executes when timer reaches max (and resets)
+void IRAM_ATTR onTimer() {
+
+  BaseType_t task_woken = pdFALSE;
+
+  // Perform action (read from ADC)
+  val = analogRead(adc_pin);
+
+  // Give semaphore to tell task that new value is ready
+  xSemaphoreGiveFromISR(bin_sem, &task_woken);
+
+  // Exit from ISR (Vanilla FreeRTOS)
+  //portYIELD_FROM_ISR(task_woken);
+
+  // Exit from ISR (ESP-IDF)
+  if (task_woken) {
+    portYIELD_FROM_ISR();
+  }
+}
+
+//*****************************************************************************
+// Tasks
+
+// Wait for semaphore and print out ADC value when received
+void printValues(void *parameters) {
+
+  // Loop forever, wait for semaphore, and print value
+  while (1) {
+    xSemaphoreTake(bin_sem, portMAX_DELAY);
+    Serial.println(val); 
+  }
+}
+
+//*****************************************************************************
+// Main (runs as its own task with priority 1 on core 1)
+
+void setup() {
+
+  // Configure Serial
+  Serial.begin(115200);
+
+  // Wait a moment to start (so we don't miss Serial output)
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS ISR Buffer Demo---");
+
+  // Create semaphore before it is used (in task or ISR)
+  bin_sem = xSemaphoreCreateBinary();
+
+  // Force reboot if we can't create the semaphore
+  if (bin_sem == NULL) {
+    Serial.println("Could not create semaphore");
+    ESP.restart();
+  }
+
+  // Start task to print out results (higher priority!)
+  xTaskCreatePinnedToCore(printValues,
+                          "Print values",
+                          1024,
+                          NULL,
+                          2,
+                          NULL,
+                          app_cpu);
+
+  // Create and start timer (num, divider, countUp)
+  timer = timerBegin(0, timer_divider, true);
+
+  // Provide ISR to timer (timer, function, edge)
+  timerAttachInterrupt(timer, &onTimer, true);
+
+  // At what count should ISR trigger (timer, count, autoreload)
+  timerAlarmWrite(timer, timer_max_count, true);
+
+  // Allow ISR to trigger
+  timerAlarmEnable(timer);
+}
+
+void loop() {
+  // Do nothing, forever
+}
